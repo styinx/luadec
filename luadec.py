c450db5fc0a602d7d95ee89e4af75a55ab9853bf
Christoph Zorn <zornchrisjo@gmail.com>
2023-05-17 21:19:58 +0200

    Start with decompiler

--- luadec.py
+++ luadec.py
@@ -2,6 +2,9 @@ import sys
 import pickle
 
 from shared import Chunk
+from iter import Iterator
+
+INT_MAX = 2147483647 - 2
 
 
 def get_OP(instruction: int) -> int:
@@ -13,7 +16,7 @@ def get_U(instruction: int) -> int:
 
 
 def get_S(instruction: int) -> int:
-    return (instruction & 0xFFFFFFC0) >> 6
+    return ((instruction & 0xFFFFFFC0) - INT_MAX) >> 6
 
 
 def get_A(instruction: int) -> int:
@@ -100,38 +103,93 @@ class OP:
     JMPF, JMPONT, JMPONF, JMP, PUSHNILJMP, FORPREP, FORLOOP, LFORPREP, LFORLOOP, CLOSURE = range(49)
 
 
-def process_closure(it: iter, chunk: Chunk):
-    i = next(it)
-    if i == OP.SETGLOBAL:
-        name = chunk.strings[get_B(i)]
+def printf(*args, indent=2):
+    print(' ' * indent, *args, end=' ')
 
-        print(f'function {name}()')
 
-        for child in chunk.functions:
-            process_chunk(child)
+def process_closure(it: Iterator, chunk: Chunk, stack, local_vars):
+    operator = it.next()
 
-        print(f'end')
+    if operator == OP.SETGLOBAL:
+        name = chunk.strings[get_B(operator)]
+        printf(f'function {name}()\n')
+    else:
+        it.prev()
+        printf(f'function ()\n')
 
+    for child in chunk.functions:
+        process_chunk(child)
+
+    operator = it.next()
+    if operator == OP.END:
+        printf(f'end')
+
+
+def process_call(it: Iterator, chunk: Chunk, stack, local_vars):
+    def map_type(args):
+        out = []
+        for arg in args:
+            if isinstance(arg, (int, float)):
+                out.append(str(arg))
+            else:
+                out.append(f'"{str(arg)}"')
+        return out
+
+    printf(f'{stack[0]}({", ".join(map_type(stack[1:]))})\n')
+
+
+def build_stack(it: Iterator, chunk: Chunk, stack, local_vars):
+    instruction = it.get()
+    operator = get_OP(instruction)
+
+    if operator == OP.GETGLOBAL:
+        stack.append(chunk.strings[get_B(instruction)])
+    elif operator == OP.GETLOCAL:
+        stack.append(local_vars[get_B(instruction) - 1])  # index starts from 1 and not 0
+    elif operator == OP.PUSHINT:
+        stack.append(get_S(instruction) + 1)  # unclear why +1 is necessary
+    elif operator == OP.PUSHNUM:
+        stack.append(chunk.numbers[get_B(instruction)])
+    elif operator == OP.PUSHNEGNUM:
+        stack.append(chunk.numbers[get_B(instruction)] * -1)
+    elif operator == OP.PUSHSTRING:
+        stack.append(chunk.strings[get_B(instruction)])
+    else:
+        print('#TODO:', OP_NAME[operator])
 
 
 HANDLE = {
-    OP.CLOSURE: process_closure
+    OP.CLOSURE: process_closure,
+    OP.CALL: process_call
 }
 
 
 def process_chunk(chunk: Chunk):
-    it = iter(chunk.instructions)
+    it = Iterator(chunk.instructions)
+    stack = []
+    local_vars = []
 
-    instruction = next(it)
-    print(instruction, OP_NAME[get_OP(instruction)])
+    # Local variable definition at the start of a chunk
+    while get_OP(it.get()) == OP.PUSHINT:
+        local = get_B(it.get())
+        local_vars.append(local)
+        it.next()
+    it.prev()
+
     try:
-        while it:
-            if instruction in HANDLE:
-                HANDLE[instruction](it, chunk)
-            instruction = next(it)
-            print(instruction, OP_NAME[get_OP(instruction)])
-    except Exception as e:
+        for instruction in it:
+            operator = get_OP(instruction)
+
+            if operator in HANDLE:
+                HANDLE[operator](it, chunk, stack, local_vars)
+                stack = []
+            else:
+                build_stack(it, chunk, stack, local_vars)
+
+    except StopIteration as e:
         pass
+    except Exception as e:
+        print(e)
 
 
 def main(file):
